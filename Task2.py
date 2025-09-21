f483eecba1d7568a17a4e24f699fc11fefaeb45c
aryanbohidar001 <aryanbohidar001@gmail.com>
2025-09-21 21:00:23 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,8 @@
+#Demonstrate List Slicing
+
+list=[1,2,3,4,5,6,7,8,9,10]
+print(f"Original list:{list}")
+a=list[0:5]
+print(f"Extracted first five elements:{a}")
+a.reverse()
+print(f"Reversed extracted elements:{a}")
